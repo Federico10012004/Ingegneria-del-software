d0974b14d29d1cef460e14316e96ecc19a0863a3
Federico.Guglielmini <federico.guglielmini@students.uniroma2.eu>
2025-10-17 21:35:36 +0200

    Fix high issues

--- src/main/java/it/uniroma2/dicii/CalcettoHub/dao/LoginDao.java
+++ src/main/java/it/uniroma2/dicii/CalcettoHub/dao/LoginDao.java
@@ -1,14 +1,14 @@
 package it.uniroma2.dicii.CalcettoHub.dao;
 
 public class LoginDao {
-    public static final String correctEmail = "admin@mail.com";
-    public static final String correctPassword = "admin";
+    public static final String CorrectEmail = "admin@mail.com";
+    public static final String CorrectPassword = "admin";
 
     public int checkCredentials(String email, String password) {
 
         if (email == null || email.isEmpty() || password == null || password.isEmpty()) {
             return -1;
-        } else if (!email.equals(correctEmail) || !password.equals(correctPassword)) {
+        } else if (!email.equals(CorrectEmail) || !password.equals(CorrectPassword)) {
             return 1;
         } else {
             return 0;
