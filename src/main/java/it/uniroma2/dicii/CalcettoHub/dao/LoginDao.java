8601acf9af3f60fb912910a22cd5d5ecf0140046
Federico.Guglielmini <federico.guglielmini@students.uniroma2.eu>
2025-10-17 21:38:26 +0200

    Fix high issues

--- src/main/java/it/uniroma2/dicii/CalcettoHub/dao/LoginDao.java
+++ src/main/java/it/uniroma2/dicii/CalcettoHub/dao/LoginDao.java
@@ -1,14 +1,14 @@
 package it.uniroma2.dicii.CalcettoHub.dao;
 
 public class LoginDao {
-    public static final String CorrectEmail = "admin@mail.com";
-    public static final String CorrectPassword = "admin";
+    public static final String Correct_Email = "admin@mail.com";
+    public static final String Correct_Password = "admin";
 
     public int checkCredentials(String email, String password) {
 
         if (email == null || email.isEmpty() || password == null || password.isEmpty()) {
             return -1;
-        } else if (!email.equals(CorrectEmail) || !password.equals(CorrectPassword)) {
+        } else if (!email.equals(Correct_Email) || !password.equals(Correct_Password)) {
             return 1;
         } else {
             return 0;
--- src/main/java/it/uniroma2/dicii/CalcettoHub/dao/RegisterDao.java
+++ src/main/java/it/uniroma2/dicii/CalcettoHub/dao/RegisterDao.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public class RegisterDao {
 
-    public static final String CorrectEmail = "admin@mail.com";
+    public static final String Correct_Email = "admin@mail.com";
 
     public int checkField(String user, LocalDate date, String email, String password, String checkPassword) {
 
@@ -12,7 +12,7 @@ public class RegisterDao {
             return 1;
         } else if (!password.equals(checkPassword)) {
             return -1;
-        } else if (email.equals(CorrectEmail)){
+        } else if (email.equals(Correct_Email)){
             return -2;
         } else {
             return 0;
